f8266a77b2bf150dc7b4793c549620a6b30fd35f
manojreddy7970 <47962043+manojreddy7970@users.noreply.github.com>
2020-12-21 16:18:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+   key = os.getenv("KEY")
+   file = os.getenv("FILE")
+   print(key)
    return 'Hello World'
 
 if __name__ == '__main__':
